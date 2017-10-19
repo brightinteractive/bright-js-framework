6322ad9a681cb7e5c098b0f5a1af096b630774fa
Chris Devereux <devereux.chris@gmail.com>
2017-10-19 14:17:45 +0100

    Fix github credentials issue

---
+++ ci/predeploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e # Exit with nonzero exit code if anything fails
+
+# Don't try to autopublish on pull requests
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo 'Pull request. Not bulding.'
+  exit 0
+fi
+
+# Build the library
+echo 'Building...'
+gulp build
+
+# Clean any build/install artifacts that might inadvertently be tracked by git
+# Built products for deploy won't be affected by this as they're not tracked
+git checkout .
+cat .git/config
+
+# Bump the npm version and tag on github
+echo 'Tag new version...'
+npm version patch
+git push --tags
+
+# Get the new version
+version=$(node -e 'console.log(require("./package.json").version)')
+
+# Push the latest package.json to master
+git add package.json
+git commit -m "$version\n[ci skip]"
+git push origin master
+
