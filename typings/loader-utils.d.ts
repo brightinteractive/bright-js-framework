dc35adc291b28fe3b96194c092129f7db624e373
Chris Devereux <devereux.chris@gmail.com>
2017-10-23 14:07:38 +0100

    AP-86: Webpack loader-utils documentation

--- typings/loader-utils.d.ts
+++ typings/loader-utils.d.ts
@@ -1,15 +1,30 @@
 declare module 'loader-utils' {
   export interface LoaderContext {
-    query: { [key: string]: string | undefined }
+    /** The query of the request for the current loader. */
+    query: string | { [key: string]: string | undefined }
+
+    /** The resource file. */
     resourcePath: string
+
+    /** The directory of the module. Can be used as context for resolving other stuff. */
     context: string
 
-    /** Add file as a dependency to the current module */
+    /** Resolve a request like a require expression */
     resolve(context: string, request: string, callback: (err: {}, result: string) => void): void
 
-    /** Add file as a dependency to the current module */
+    /** Add a file as dependency of the loader result in order to make them watchable */
     addDependency(file: string): void
+
+    /**
+     * Make this loader result cacheable. By default it’s not cacheable.
+
+      A cacheable loader must have a deterministic result, when inputs and dependencie
+      haven’t changed. This means the loader shouldn’t have other dependencies than
+      specified with this.addDependency. Most loaders are deterministic and cacheable.
+     */
+    cacheable(flag?: boolean): void
   }
 
+  /** Recommended way to retrieve the options of a loader invocation */
   export function getOptions(ctx: LoaderContext): any
 }
