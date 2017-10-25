65c1a29427d7bba5fee6eae76da7594debbfc75e
Chris Devereux <devereux.chris@gmail.com>
2017-10-25 11:43:43 +0100

    Doc site styling improvements

--- docs-site/src/components/Section.js
+++ docs-site/src/components/Section.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Card, CardHeader, CardText } from 'material-ui'
 
 export const Section = ({ title, children, alwaysExpanded }) => (
-  <Card>
+  <Card style={{ marginBottom: '1em' }}>
     <CardHeader showExpandableButton={!alwaysExpanded} actAsExpander={!alwaysExpanded} title={title} />
     <CardText expandable={!alwaysExpanded}>
       {children}
