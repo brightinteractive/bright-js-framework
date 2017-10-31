0ce776b7dd88cf47fb619b0bb2647f32fd58880e
Chris Devereux <devereux.chris@gmail.com>
2017-10-31 11:14:50 +0000

    CLI support for global install

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,8 +1,49 @@
 #!/usr/bin/env node
 
+import * as path from 'path'
+import * as fs from 'fs'
 import * as yargs from 'yargs'
 import { runCommand } from './run.command'
+import { initCommand } from './init.command'
 
-yargs
+if (localCliIsRunning()) {
+  localCli()
+
+} else if (localCliExists()) {
+  runLocalCli()
+
+} else {
+  globalCli()
+}
+
+function localCliExists() {
+  try {
+    fs.statSync(localCliPath)
+    return true
+
+  } catch {
+    return false
+  }
+}
+
+function localCliIsRunning() {
+  return path.normalize(localCliPath) === path.normalize(__filename)
+}
+
+function runLocalCli() {
+  require(localCliPath)
+}
+
+function localCli() {
+  yargs
   .command(runCommand)
   .argv
+}
+
+function globalCli() {
+  yargs
+  .command(initCommand)
+  .argv
+}
+
+const localCliPath = path.resolve(path.join('node_modules', '@brightinteractive', 'bright-js-framework', 'cli', 'cli.js'))
