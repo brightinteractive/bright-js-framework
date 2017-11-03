4f5e249b1cffce2432220580777b622d32e07a27
Chris Devereux <devereux.chris@gmail.com>
2017-11-03 17:29:16 +0000

    AP-87: Add dependency injection API

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ import { decorateRouteComponent } from './lib/core/route'
 import { decorateController } from './lib/core/Controller'
 import { Service as _Service, decorateServiceProperty } from './lib/core/Service'
 import { Link as _Link } from './lib/components/Link'
+import { PluginConfig as _PluginConfig, exportDependency } from './lib/core/PluginConfig'
+import { injectDependency } from './lib/core/InjectionClient'
 
 /**
  * Declare a component as a route and associate a path with it.
@@ -97,8 +99,10 @@ export function controller(): ComponentDecorator {
  *  }
  * ```
  */
-export function service(constructor: new () => Service): PropertyDecorator {
-  return decorateServiceProperty(constructor)
+export function service(constructor: typeof Service): PropertyDecorator {
+  // This cast to any is annoying but necessary as we don't want to leak private type information
+  // about the Service class. This API will be covered by integration tests when we write them.
+  return decorateServiceProperty(constructor as any)
 }
 
 /**
@@ -135,7 +139,43 @@ export interface Service<State = {}> {
   setState(state: Partial<State>): void
 }
 
-export const Service: new <State>() => Service<State> = _Service
+export const Service: new <State>(context: ServiceContext) => Service<State> = _Service
+
+/** Opaque object passed into service constructors */
+export interface ServiceContext {
+  '@appContext': any
+}
+
+/**
+ * Configuration class for providing additonal features (such as data-fetching, authentication
+ * providers, etc) to an application.
+ *
+ * Plugins declare dependencies by using the @exported decorator.
+ *
+ * @class
+ */
+export interface PluginConfig extends Service {}
+export const PluginConfig = _PluginConfig
+
+/**
+ * Declare that a property of a PluginConfig is available for injection into components
+ * and services.
+ *
+ * @param id Identifier used to inject the dependency when provided to @inject decorator.
+ */
+export function exported(id: string): PropertyDecorator {
+  return exportDependency(id)
+}
+
+/**
+ * Declare that a property of a Controller, Service or PluginContext should be fulfilled
+ * using dependency injection.
+ *
+ * @param id Identifier of the dependency to inject.
+ */
+export function inject(id: string): PropertyDecorator {
+  return injectDependency(id)
+}
 
 export type ComponentDecorator<Props = {}> = (cls: React.ComponentClass<Props>) => void
 export type PropertyDecorator = (proto: any, key: string) => any
