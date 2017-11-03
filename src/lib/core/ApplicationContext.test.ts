d2bbf4618efad4daf09f2f8e94d177168107c25f
Chris Devereux <devereux.chris@gmail.com>
2017-11-03 16:50:46 +0000

    AP-87: Determine required order of plugin creation before creating plugins

--- src/lib/core/ApplicationContext.test.ts
+++ src/lib/core/ApplicationContext.test.ts
@@ -1,58 +1,15 @@
 import { expect } from 'chai'
-import { uniqueId } from 'lodash'
-import { Plugin } from './Plugin'
+import { Plugin, exportDependency } from './Plugin'
 import { ApplicationContext } from './ApplicationContext'
 
 describe('ApplicationContext', () => {
   class MyPlugin extends Plugin {
-    injectedObjects = {
-      object: () => uniqueId('injected')
-    }
-
-    storeReducers = {
-      value: (state = 0, action: any) => {
-        if (action.type === 'set-value') {
-          return action.value
-
-        } else {
-          return state
-        }
-      }
-    }
-
-    storeMiddlewares = [
-      () => (next: any) => (action: any) => {
-        if (action.type === 'to-infinity')  {
-          return next({ type: 'set-value', value: Infinity })
-
-        } else {
-          return next(action)
-        }
-      }
-    ]
+    @exportDependency('myDependency')
+    foo = 1
   }
 
   it('should return injected objects', () => {
     const ctx = new ApplicationContext([MyPlugin])
-    expect(ctx.injectedObjects.object).to.include('injected')
-  })
-
-  it('should memoize construction of injected objects', () => {
-    const ctx = new ApplicationContext([MyPlugin])
-    expect(ctx.injectedObjects.object).to.eql(ctx.injectedObjects.object)
-  })
-
-  it('should apply reducers to store', () => {
-    const ctx = new ApplicationContext([MyPlugin])
-
-    ctx.store.dispatch({ type: 'set-value', value: 5 })
-    expect(ctx.store.getState().value).to.eql(5)
-  })
-
-  it('should apply middlewares to store', () => {
-    const ctx = new ApplicationContext([MyPlugin])
-
-    ctx.store.dispatch({ type: 'to-infinity' })
-    expect(ctx.store.getState().value).to.eql(Infinity)
+    expect(ctx.injectedObjects.myDependency).to.eql(1)
   })
 })
--- src/lib/core/ApplicationContext.ts
+++ src/lib/core/ApplicationContext.ts
@@ -1,55 +1,20 @@
-import * as Redux from 'redux'
-import { identity, constant, mapValues, memoize, isEmpty } from 'lodash'
-import { PluginConstructor, Plugin, combinePlugins, ContextValueMap, ContextFactoryMap } from './Plugin'
+import { PluginConstructor, ContextValueMap, getPluginCreationOrder, getExportedDependencies } from './Plugin'
 
 export class ApplicationContext {
-  readonly store: Redux.Store<any>
-  readonly injectedObjects: ContextValueMap
+  readonly injectedObjects: ContextValueMap = {}
 
   constructor(pluginConstructors: PluginConstructor[] = []) {
-    const plugins = pluginConstructors.map((Constructor) => new Constructor())
-    const plugin = combinePlugins(plugins)
-
-    this.injectedObjects = createContextValueMap(plugin.injectedObjects)
-    this.store = createStoreInstance(plugin)
-  }
-}
-
-/**
- * Given a map of factory functions for injected objects, return an object mapping keys
- * to lazily constructed objects suitable for passing down the React context tree to
- * controllers and services.
- *
- * Specifying dependencies as memoized factory functions rather than raw objects is important,
- * as it allows plugins to depend on other plugins for dependencies.
- */
-export function createContextValueMap(factories: ContextFactoryMap): ContextValueMap {
-  return Object.defineProperties({}, mapValues(factories, (factory) => ({
-    get: memoize(factory)
-  })))
-}
-
-export function createStoreInstance(plugin: Plugin) {
-  const reducer = isEmpty(plugin.storeReducers) ? constant({}) : Redux.combineReducers(plugin.storeReducers)
-  const middleware = Redux.applyMiddleware(...plugin.storeMiddlewares)
-
-  return Redux.createStore(reducer, Redux.compose(middleware, getDevtools()))
-}
-
-/**
- * Return redux devtools if the chrome extension is installed and we aren't
- * running in production
- */
-function getDevtools() {
-  if (process.env.NODE_ENV === 'production') {
-    return identity
+    const orderedConstructors = getPluginCreationOrder(pluginConstructors)
+    orderedConstructors.forEach((constructor) => this.constructPlugin(constructor))
   }
 
-  return (
-    typeof __REDUX_DEVTOOLS_EXTENSION__ !== 'undefined'
-    ? __REDUX_DEVTOOLS_EXTENSION__()
-    : identity
-  )
-}
+  private constructPlugin(PluginType: PluginConstructor) {
+    const instance = new PluginType({
+      '@appContext': this
+    })
 
-declare const __REDUX_DEVTOOLS_EXTENSION__: (() => Redux.GenericStoreEnhancer) | undefined
+    getExportedDependencies(PluginType).forEach(({ dependencyId, propertyKey }) => {
+      this.injectedObjects[dependencyId] = (instance as any)[propertyKey]
+    })
+  }
+}
