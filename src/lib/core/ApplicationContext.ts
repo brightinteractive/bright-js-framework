0ebf364776c6e9ab80a3eac3b9585ca8d1793757
Chris Devereux <devereux.chris@gmail.com>
2017-11-07 14:16:30 +0000

    AP-155: Implement TestFixture

--- src/lib/core/ApplicationContext.ts
+++ src/lib/core/ApplicationContext.ts
@@ -1,7 +1,8 @@
-import { PluginConstructor, ContextValueMap, getPluginCreationOrder, getExportedDependencies } from './PluginConfig'
+import { PluginConstructor, ContextValueMap, getPluginCreationOrder, getExportedDependencies, PluginConfig } from './PluginConfig'
 
 export class ApplicationContext {
   readonly injectedObjects: ContextValueMap = {}
+  readonly plugins: PluginConfig[] = []
 
   constructor(pluginConstructors: PluginConstructor[] = []) {
     const orderedConstructors = getPluginCreationOrder(pluginConstructors)
@@ -12,6 +13,7 @@ export class ApplicationContext {
     const instance = new PluginType({
       '@appContext': this
     })
+    this.plugins.push(instance)
 
     getExportedDependencies(PluginType).forEach(({ dependencyId, propertyKey }) => {
       this.injectedObjects[dependencyId] = (instance as any)[propertyKey]
