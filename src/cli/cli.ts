cfa17b2bf78023c5c2d9b526348962ee59049ff1
Chris Devereux <devereux.chris@gmail.com>
2017-11-22 10:41:40 +0000

    AP-97: Fix CLI behavior when locally linked

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,51 +1,17 @@
-#!/usr/bin/env node
-
 import * as path from 'path'
-import * as fs from 'fs'
 import * as yargs from 'yargs'
 
-if (localCliIsRunning()) {
-  localCli()
-
-} else if (localCliExists()) {
-  runLocalCli()
-
-} else {
-  globalCli()
-}
-
-function localCliExists() {
-  try {
-    fs.statSync(localCliPath())
-    return true
-
-  } catch {
-    return false
-  }
-}
-
-function localCliIsRunning() {
-  return path.normalize(localCliPath()) === path.normalize(__filename)
-}
-
-function runLocalCli() {
-  require(localCliPath())
-}
-
-function localCli() {
+export function runLocalCli() {
   yargs
-  .commandDir(path.resolve(__dirname, 'local-commands'))
-  .help()
-  .argv
+    .commandDir(path.resolve(__dirname, 'local-commands'))
+    .help()
+    .argv
 }
 
-function globalCli() {
+export function runGlobalCli() {
   yargs
-  .commandDir(path.resolve(__dirname, 'global-commands'))
-  .help()
-  .argv
-}
-
-function localCliPath() {
-  return path.resolve(path.join('node_modules', '@brightinteractive', 'bright-js-framework', 'cli', 'cli.js'))
+    .commandDir(path.resolve(__dirname, 'global-commands'))
+    .commandDir(path.resolve(__dirname, 'local-commands'))
+    .help()
+    .argv
 }
