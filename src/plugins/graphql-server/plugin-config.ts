47579c915788c377a9c3f73fe35fb72ca67f95d6
Chris Devereux <devereux.chris@gmail.com>
2017-12-06 19:12:00 +0000

    Distinguish between client and server environments when loading plugins

--- src/plugins/graphql-server/plugin-config.ts
+++ src/plugins/graphql-server/plugin-config.ts
@@ -2,6 +2,7 @@ import * as path from 'path'
 import * as glob from 'glob'
 import { findGraphQLSources } from '../../lib/plugins/GraphQLServerPlugin/loadSchema'
 import { EntrypointOpts } from '../../lib/bundler/entrypointLoader'
+import { PluginLoaderOpts } from '../../lib/bundler/PluginLoader'
 
 const pluginFactory = require.resolve('../../lib/plugins/GraphQLServerPlugin/graphQLServerPluginEntry')
 
@@ -11,7 +12,11 @@ const pluginFactory = require.resolve('../../lib/plugins/GraphQLServerPlugin/gra
  * Adds GraphQL sources into the application bundle and passes them into the plugin
  * factory on load.
  */
-export default function graphQlPluginConfig(): EntrypointOpts {
+export default function graphQlPluginConfig(opts: {}, { environment }: PluginLoaderOpts): EntrypointOpts | undefined {
+  if (environment !== 'server') {
+    return undefined
+  }
+
   const topLevelModules = findGraphQLSources({
     glob: glob.sync,
     resolvePath: (pathname) => path.resolve(pathname)
