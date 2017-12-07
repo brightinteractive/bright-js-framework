a8149246241836bd36da29e2de52afbec15c97f1
Chris Devereux <devereux.chris@gmail.com>
2017-12-07 12:14:50 +0000

    Add schema type generation support

--- src/plugins/graphql-server/plugin-config.ts
+++ src/plugins/graphql-server/plugin-config.ts
@@ -1,5 +1,7 @@
 import * as path from 'path'
 import * as glob from 'glob'
+import filewatcher = require('filewatcher')
+import { introspectSchema, generate } from 'apollo-codegen'
 import { findGraphQLSources } from '../../lib/plugins/GraphQLServerPlugin/loadSchema'
 import { EntrypointOpts } from '../../lib/bundler/entrypointLoader'
 import { PluginLoaderOpts } from '../../lib/bundler/PluginLoader'
@@ -22,5 +24,47 @@ export default function graphQlPluginConfig(opts: {}, { environment }: PluginLoa
     resolvePath: (pathname) => path.resolve(pathname)
   })
 
+  if (process.env.NODE_ENV !== 'production') {
+    startSchemaGenerator()
+  }
+
   return { entry: pluginFactory, topLevelModules }
+
+  async function startSchemaGenerator() {
+    await regenerateSchemas()
+
+    const watcher = filewatcher()
+    glob.sync('src/**/*.graphql').forEach((file) => {
+      watcher.add(file)
+    })
+
+    watcher.on('change', regenerateSchemas)
+  }
+
+  async function regenerateSchemas() {
+    if (!topLevelModules.schemas[0]) {
+      return
+    }
+
+    await introspectSchema(topLevelModules.schemas[0].typeDefs, './schema.json')
+
+    glob.sync('src/**/queries').forEach((sourcePath) => {
+      generate(
+        glob.sync(`${sourcePath}/*.graphql`).map((x) => path.resolve(x)),
+        path.resolve('./schema.json'),
+        path.resolve(`${sourcePath}/types.ts`),
+        'typescript',
+        'gql',
+        {
+          passthroughCustomScalars: false,
+          customScalarsPrefix: '',
+          addTypename: false,
+          namespace: '',
+          operationIdsPath: null,
+          generateOperationIds: false,
+          mergeInFieldsFromFragmentSpreads: true
+        }
+      )
+    })
+  }
 }
